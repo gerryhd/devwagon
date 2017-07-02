ec70134cfb3387093ddfd8031a0e338682bcf0bb
Gerry <gerry_hd@live.com.mx>
2017-07-02 02:46:42 -0500

    Setting it up, removed database

--- devwagon/urls.py
+++ devwagon/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from qforum import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^qforum/', include('qforum.urls')),
+    url(r'^$', include('qforum.urls')),
     url(r'^admin/', admin.site.urls),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- qforum/views.py
+++ qforum/views.py
@@ -6,4 +6,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to devwagon!")
+    context_dict = {}
+
+    return render(request, 'qforum/index.html', context_dict)
